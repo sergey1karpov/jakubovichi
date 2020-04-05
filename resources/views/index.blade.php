5009c1fe3f106d3bcbbfdfff674eedee44cb2a27
Sergey <karpovsergey1990@gmail.com>
2020-04-05 11:55:06 +0400

    jakjakjak Some fix 1.5

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
         <hr>
         <img src="{{$post->img_link ?? $post->img}}" class="img-fluid mt-3 mb-3 text-center" style="width: 800px;">
         <p class="mt-3 mb-3 ml-3 mr-5 text-center">{{$post->short_text}}</p>
-        <p class="blockquote-footer mt-3 mb-3 ml-3 mr-5">{{$post->created_at}} | Автор:
+        <p class="blockquote-footer mt-3 mb-3 ml-3 mr-5">{{$post->created_at->diffForHumans()}} | Автор:
             <cite title="Название источника">Ярмольник Л. | Комментариев: {{$post->comment->count()}} | </cite>
             <a href="{{route('showPost', ['id' => $post->id])}}">Читать новость про Леонида Аркадиевича</a>
         </p>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -14,22 +14,30 @@
     </div>
 </div>
 
-<div class="row mt-5 text-center">
-    <div class="col-12 text-center">
-        <h3 class=" mb-3   text-center display-4" style="font-size: 40px">Добавить комментарий</h3>
+@if(Auth::check())
+    <div class="row mt-5 text-center">
+        <div class="col-12 text-center">
+            <h3 class=" mb-3   text-center display-4" style="font-size: 40px">Добавить комментарий</h3>
+        </div>
     </div>
-</div>
 
-<div class="row text-center">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xl-12 text-center">
-        <form action="{{route('addComment', ['id'=>$post->id])}}" method="post" class="form-group">
-            @csrf
-            <textarea name="comment" cols="55" rows="5"></textarea><br>
-            <input type="hidden" value="{{$post->id}}" name="post_id">
-            <input type="submit" value="Добавить комментарий">
-        </form>
+    <div class="row text-center">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xl-12 text-center">
+            <form action="{{route('addComment', ['id'=>$post->id])}}" method="post" class="form-group">
+                @csrf
+                <textarea name="comment" cols="55" rows="5"></textarea><br>
+                <input type="hidden" value="{{$post->id}}" name="post_id">
+                <input type="submit" value="Добавить комментарий">
+            </form>
+        </div>
     </div>
-</div>
+@else
+    <div class="row mt-5 text-center">
+        <div class="col-12 text-center">
+            <h3 class="    text-center display-4" style="font-size: 20px">Что бы прокоментировать эту прекрасную новость вам нужно зарагистрироваться</h3>
+        </div>
+    </div>
+@endif
 
 <hr>
 <div class="row mt-5 text-center">
@@ -43,11 +51,15 @@
         @foreach($post->comment as $comment)
             <h3 class=" mb-3   text-center display-4" style="font-size: 20px">- "{{$comment->comment}}"</h3>
             <h3 class=" mb-3  text-center display-4" style="font-size: 15px">Прокоментировал {{$comment->author_name}} {{$comment->created_at->diffForHumans()}}
-            <form action="{{route('deleteComment', ['id' => $comment->id, '$comment' =>$comment->user_id])}}" method="post" class="mt-2">
-                @csrf @method('DELETE')
-                <input type="submit" value="удалить">
-            </form></h3>
-            <h5 class=" mb-3 text-center">* * *</h5>
+            @if(Auth::check())
+                @if(Auth::user()->id == $comment->user_id)
+                        <form action="{{route('deleteComment', ['id' => $comment->id, '$comment' =>$comment->user_id])}}" method="post" class="mt-2">
+                            @csrf @method('DELETE')
+                            <input type="submit" value="удалить">
+                        </form></h3>
+                @endif
+            @endif
+
         @endforeach
     </div>
 </div>
